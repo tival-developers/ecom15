61a5ec08a61aab47e46aa3bbd8f424737da7e165
tival-developers <ngigecollins630@gmail.com>
2025-02-15 18:21:35 +0300

    03-create-website-layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div>
+      <h1 className='h1-bold text-center p-10'>Home</h1>
+    </div>
+  )
+}
---
+++ lib/data.ts
@@ -0,0 +1,87 @@
+const data ={
+    headerMenus: [
+        {
+          name: "Today's Deals",
+          href: "/search?tag=todays-deals",
+        },
+
+        {
+          name: "New Arrivals",
+          href: "/search?tag= new-arrivals",
+        },
+
+        {
+          name: "Featured Products",
+          href: "/search?tag=featured-products",
+        },
+
+        {
+            name: "Best Sellers",
+            href: "/search?tag=best-sellers",
+        },
+
+        {
+            name: "Browsing History",
+            href: "/#browsing-history",
+        },
+
+        {
+            name: "Customer Service",
+            href: "/page/customer-service",
+        },
+
+        {
+            name: "About Us",
+            href: "/page/about-us",
+        },
+
+        {
+            name: "Contact Us",
+            href: "/page/contact-us",
+        },
+
+        {
+            name: "Help",
+            href: "/page/help",
+        },
+
+        {
+            name: "Return & Refund",
+            href: "/page/return-refund",
+        },
+
+        {
+            name: "Payment Options",
+            href: "/page/payment-options",
+        },
+
+        {
+            name: "Shipping & Delivery",
+            href: "/page/shipping-delivery",
+        },
+
+        {
+            name: "Privacy Policy",
+            href: "/page/privacy-policy",
+        },
+
+        {
+            name: "Terms & Conditions",
+            href: "/page/terms-conditions",
+        },
+
+        {
+            name: "Gift Cards",
+            href: "/page/gift-cards",
+        },
+
+        {
+        name: "FAQs",
+        href: "/page/faqs",
+        },
+
+      ],
+        
+}
+
+export default data
